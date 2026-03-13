51549f33131853d1eb426c693deb83d1c766bd9a
vo nhat minh <himinhgit23@gmail.com>
2026-03-13 22:10:50 +0700

    Add AboutUs component

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div>
+      <h1>About Paradise Nursery</h1>
+      <p>
+        Paradise Nursery is a plant shop that offers a wide variety of beautiful
+        and healthy houseplants. Our mission is to bring nature closer to people
+        by providing affordable, decorative, and easy-to-care-for plants for
+        homes and workplaces.
+      </p>
+      <p>
+        We believe that plants make every space fresher, greener, and more
+        relaxing. At Paradise Nursery, customers can explore different plant
+        categories and choose the perfect plants for their lifestyle.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
